f926456e29f4b649d7983a51347b3a1182014230
heavy <flowvlad@gmail.com>
2017-03-16 17:41:44 +0100

    Final fixing of navigation page

---
+++ Web_Library/Scripts/IndexScript.js
@@ -0,0 +1,10 @@
+﻿$("#right-link").hover(function () {
+    $("#left-link").addClass("hover");
+}, function () {
+    $("#left-link").removeClass("hover");
+});
+$("#left-link").hover(function () {
+    $("#right-link").addClass("hover");
+}, function () {
+    $("#right-link").removeClass("hover");
+});
