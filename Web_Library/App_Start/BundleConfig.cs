24997250c7cdffbcce08828655101284428da6a7
heavy <flowvlad@gmail.com>
2017-03-30 19:04:08 +0200

    Added style for Books View

--- Web_Library/App_Start/BundleConfig.cs
+++ Web_Library/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace Web_Library
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/font-awesome.css"));
         }
     }
 }
